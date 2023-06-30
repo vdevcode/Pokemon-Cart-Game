b4d0ff1aa38df239f4b7df2716952584662757a3
vdevcode <thanhvumaidao123456@gmail.com>
2023-06-30 13:27:46 +0700

    upload

--- src/components/ResultScreen.vue
+++ src/components/ResultScreen.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="screen-card">
+  <div class="screen">
     <h1>✨ Congratulations ✨</h1>
     <h3>{{ Math.round(timer / 920) }} seconds</h3>
     <button @click="onStartAgain">Start Again</button>
@@ -22,4 +22,48 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.screen {
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  background-color: var(--dark);
+  color: var(--light);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.screen h1 {
+  font-size: 5rem;
+  color: yellow;
+}
+
+.screen h3 {
+  margin-top: 1.5rem;
+  font-size: 3rem;
+}
+
+.screen button {
+  font: var(--font);
+  background: transparent;
+  box-shadow: none;
+  border: 1px solid var(--light);
+  color: var(--light);
+  margin: 1rem;
+  padding: 1rem 1.25rem;
+  border-radius: 0.5rem;
+  font-size: 1.25rem;
+  cursor: pointer;
+  transition: background 0.3s ease-in-out;
+}
+
+.screen button:hover {
+  background-color: var(--light);
+  color: var(--dark);
+}
+</style>
